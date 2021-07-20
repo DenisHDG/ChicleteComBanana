e44c516955e890843e580bba6c7a59382480e135
Denis <denishdg@gmail.com>
2021-07-20 20:31:13 -0300

    Fizemos um esquema de eleição.

---
+++ app/src/main/java/com/denismobile/chicletecombanana/Eleicao.java
@@ -0,0 +1,14 @@
+package com.denismobile.chicletecombanana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Eleicao extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_eleicao);
+    }
+}
